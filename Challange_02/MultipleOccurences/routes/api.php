4033849adfcb516caf306c0edd7248080243adec
Ravindu Jayasingha <ravindu.jayasingha@arimaclanka.com>
2022-09-29 13:49:17 +0530

    Endpoint created in routes

--- Challange_02/MultipleOccurences/routes/api.php
+++ Challange_02/MultipleOccurences/routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/get/most/occurrences', 'App\Http\Controllers\ServiceController@returnMultipleOccurrence');
